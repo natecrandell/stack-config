1d6f28ffbe9f799f1e709461c5f77e0ca076604e
Nate Crandell <nate.crandell@imaginelearning.com>
2024-01-03 13:28:17 -0700

    Don't use grep -c

--- hole-1/lograft/lograft.sh
+++ hole-1/lograft/lograft.sh
@@ -45,7 +45,7 @@ main() {
   set_vars "$1"
 
   # Check for hits
-  hit_ip_count=$(cat ${log_source} | grep -E "${date_filter}" | grep "${hit_list}" | cut -d: -f4- | cut -d' ' -f3- | grep ' from ' | cut -d' ' -f3 | sort | uniq | grep -civ "${safe_list}" 2> /dev/null)
+  hit_ip_count=$(cat ${log_source} | grep -E "${date_filter}" | grep "${hit_list}" | cut -d: -f4- | cut -d' ' -f3- | grep ' from ' | cut -d' ' -f3 | sort | uniq | grep -iv "${safe_list}" | wc -l 2> /dev/null)
   hit_ip_count_retcode=$?
   [[ ${hit_ip_count_retcode} -ne 0 ]] && die "Failed to check for hit IPs. retcode=${hit_ip_count_retcode}"
 
